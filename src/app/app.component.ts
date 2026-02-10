3367abf085235aaa4399bfa47bd507e4fd71f129
myun11 <yun.michael.ds@gmail.com>
2026-02-10 16:40:58 -0500

    Initial template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
